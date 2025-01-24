a2d77c853b04f5931126863abafd253be445bd62
divyadeep10 <divyadeepsundriyal@gmail.com>
2025-01-25 00:35:13 +0530

    first commit 12

--- code-analysis/src/App.jsx
+++ code-analysis/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
     setOutput("Processing your request...");
 
     try {
-      const response = await fetch(`http://127.0.0.1:5000${urlMap[action]}`, {
+      const response = await fetch(`https://ai-detect-backend-2h6392xmc-divyadeep10s-projects.vercel.app${urlMap[action]}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
